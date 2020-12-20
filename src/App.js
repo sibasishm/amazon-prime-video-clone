d110c52910f7d14bbe68c7acd3e080dd4e051804
Sibasish Mohanty <smsibasish@gmail.com>
2020-12-20 12:02:13 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ function App() {
 					display: 'flex',
 					justifyContent: 'space-between',
 					alignItems: 'center',
-					background: theme.text,
-					color: theme.bg,
+					background: theme.bgOffset,
 					padding: '0.75rem 0.5rem',
 				}}
 			>
@@ -30,6 +29,17 @@ function App() {
 			</header>
 			<main>
 				<h1>Hello World</h1>
+				<button
+					css={{
+						padding: '1rem 1.75rem',
+						backgroundColor: theme.primary,
+						color: theme.text,
+						borderRadius: '2px',
+						fontWeight: 600,
+					}}
+				>
+					Start your 30-day free trial
+				</button>
 			</main>
 			<footer>I am a footer</footer>
 		</div>
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,8 +1,8 @@
-export const bg = '#fafaf9';
-export const bgOffset = '#e7e5e4';
-export const text = '#1c1917';
-export const textOffset = '#404040';
-export const border = '#737373';
-export const primary = '#059669';
-export const primaryOffset = '#34d399';
-export const secondary = '#d946ef';
+export const bg = '#0F172A';
+export const bgOffset = '#1E293B';
+export const text = '#F8FAFC';
+export const textOffset = '#F1F5F9';
+export const border = '#CBD5E1';
+export const primary = '#0F79AF';
+export const primaryOffset = '#0891B2';
+export const secondary = '#059669';
